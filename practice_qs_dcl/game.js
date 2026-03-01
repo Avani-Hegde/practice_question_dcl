b6af89dcacbabcebd40dd8a950eb4cc94feae56a
AVANI HEGDE . <avanihegde684@gmail.com>
2026-03-01 23:03:16 +0530

    Add files via upload

---
+++ practice_qs_dcl/game.js
@@ -0,0 +1,8 @@
+let a=prompt("enter the game number:")
+let gameNum=25
+console.log(gameNum)
+ while(a!=gameNum){
+ a=   prompt("you entered wrong num, try again")
+
+ }
+ console.log("you entered right num")
